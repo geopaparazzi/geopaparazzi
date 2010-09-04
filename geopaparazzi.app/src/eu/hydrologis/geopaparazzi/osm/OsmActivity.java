0972798927556ac66b0e7a63b13e1a1e55646760
Andrea Antonello O <andrea.antonello@gmail.com>
2010-09-04 10:07:31 +0200

    remove google maps related stuff

--- geopaparazzi.app/src/eu/hydrologis/geopaparazzi/osm/OsmActivity.java
+++ geopaparazzi.app/src/eu/hydrologis/geopaparazzi/osm/OsmActivity.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Paint;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Menu;
@@ -35,10 +34,8 @@ import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 import eu.hydrologis.geopaparazzi.R;
 import eu.hydrologis.geopaparazzi.database.DaoNotes;
-import eu.hydrologis.geopaparazzi.gps.GpsLocation;
 import eu.hydrologis.geopaparazzi.util.ApplicationManager;
 import eu.hydrologis.geopaparazzi.util.Constants;
 
@@ -52,9 +49,8 @@ public class OsmActivity extends Activity {
     private static final int MENU_MAPDATA = 4;
     private static final int MENU_TOGGLE_MEASURE = 5;
     private static final int MENU_GPXCONVERT = 6;
-    private static final int MENU_GMAP = 7;
-    private static final int MENU_ADDTAGS = 8;
-    private static final int MENU_REMOVE_LAST_NOTE = 9;
+    private static final int MENU_ADDTAGS = 7;
+    private static final int MENU_REMOVE_LAST_NOTE = 8;
 
     private OsmView osmView;
 
@@ -65,7 +61,8 @@ public class OsmActivity extends Activity {
         // main frame
         FrameLayout frameLayout = new FrameLayout(this);
         frameLayout.setId(-9999);
-        frameLayout.setLayoutParams(new FrameLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+        frameLayout.setLayoutParams(new FrameLayout.LayoutParams(LayoutParams.FILL_PARENT,
+                LayoutParams.FILL_PARENT));
 
         // map view
         osmView = new OsmView(this);
@@ -75,13 +72,15 @@ public class OsmActivity extends Activity {
 
         // button view
         LinearLayout buttonsLayout = new LinearLayout(this);
-        buttonsLayout.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+        buttonsLayout.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT,
+                LayoutParams.FILL_PARENT));
         buttonsLayout.setPadding(5, 5, 5, 0);
         buttonsLayout.setOrientation(LinearLayout.VERTICAL);
         buttonsLayout.setGravity(Gravity.BOTTOM);
 
         TransparentPanel transparentPanel = new TransparentPanel(this);
-        transparentPanel.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+        transparentPanel.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT,
+                LayoutParams.WRAP_CONTENT));
         transparentPanel.setPadding(0, 0, 0, 0);
         buttonsLayout.addView(transparentPanel);
 
@@ -95,8 +94,8 @@ public class OsmActivity extends Activity {
                 osmView.zoomOut();
             }
         });
-        android.widget.RelativeLayout.LayoutParams paramsOut = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
-                LayoutParams.WRAP_CONTENT);
+        android.widget.RelativeLayout.LayoutParams paramsOut = new RelativeLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         paramsOut.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
         zoomoutButton.setLayoutParams(paramsOut);
 
@@ -110,13 +109,13 @@ public class OsmActivity extends Activity {
                 osmView.zoomIn();
             }
         });
-        android.widget.RelativeLayout.LayoutParams paramsIn = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
-                LayoutParams.WRAP_CONTENT);
+        android.widget.RelativeLayout.LayoutParams paramsIn = new RelativeLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         paramsIn.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
         zoominButton.setLayoutParams(paramsIn);
 
         transparentPanel.addView(zoominButton);
-        
+
         frameLayout.addView(buttonsLayout);
 
         setContentView(frameLayout);
@@ -133,15 +132,21 @@ public class OsmActivity extends Activity {
 
     public boolean onCreateOptionsMenu( Menu menu ) {
         super.onCreateOptionsMenu(menu);
-        menu.add(Menu.NONE, MENU_GPSDATA, 1, R.string.mainmenu_gpsdataselect).setIcon(R.drawable.ic_menu_compass);
-        menu.add(Menu.NONE, MENU_MAPDATA, 2, R.string.mainmenu_mapdataselect).setIcon(R.drawable.ic_menu_compass);
-        menu.add(Menu.NONE, MENU_ADDTAGS, 3, R.string.mainmenu_addtags).setIcon(R.drawable.ic_menu_add);
-        menu.add(Menu.NONE, CENTER_ON_GPS, 4, R.string.centerongps).setIcon(R.drawable.menu_mylocation);
-        menu.add(Menu.NONE, MENU_TOGGLE_MEASURE, 5, R.string.mainmenu_togglemeasure).setIcon(R.drawable.ic_menu_measure);
-        menu.add(Menu.NONE, MENU_REMOVE_LAST_NOTE, 6, R.string.mainmenu_removelastnote).setIcon(R.drawable.ic_menu_remove);
-        menu.add(Menu.NONE, MENU_GPXCONVERT, 7, R.string.mainmenu_gpxconvert).setIcon(R.drawable.gpxconvert);
+        menu.add(Menu.NONE, MENU_GPSDATA, 1, R.string.mainmenu_gpsdataselect).setIcon(
+                R.drawable.ic_menu_compass);
+        menu.add(Menu.NONE, MENU_MAPDATA, 2, R.string.mainmenu_mapdataselect).setIcon(
+                R.drawable.ic_menu_compass);
+        menu.add(Menu.NONE, MENU_ADDTAGS, 3, R.string.mainmenu_addtags).setIcon(
+                R.drawable.ic_menu_add);
+        menu.add(Menu.NONE, CENTER_ON_GPS, 4, R.string.centerongps).setIcon(
+                R.drawable.menu_mylocation);
+        menu.add(Menu.NONE, MENU_TOGGLE_MEASURE, 5, R.string.mainmenu_togglemeasure).setIcon(
+                R.drawable.ic_menu_measure);
+        menu.add(Menu.NONE, MENU_REMOVE_LAST_NOTE, 6, R.string.mainmenu_removelastnote).setIcon(
+                R.drawable.ic_menu_remove);
+        menu.add(Menu.NONE, MENU_GPXCONVERT, 7, R.string.mainmenu_gpxconvert).setIcon(
+                R.drawable.gpxconvert);
         menu.add(Menu.NONE, GO_TO, 8, R.string.goto_coordinate).setIcon(R.drawable.menu_goto);
-        menu.add(Menu.NONE, MENU_GMAP, 9, R.string.googleview).setIcon(R.drawable.menu_mapmode);
         return true;
     }
 
@@ -190,21 +195,6 @@ public class OsmActivity extends Activity {
             browseIntent.putExtra(Constants.INTENT_ID, Constants.GPXIMPORT);
             browseIntent.putExtra(Constants.EXTENTION, ".gpx"); //$NON-NLS-1$
             startActivity(browseIntent);
-            return true;
-
-            // importFromGpx();
-            // return true;
-        case MENU_GMAP:
-            GpsLocation loc = ApplicationManager.getInstance().getLoc();
-            if (loc == null) {
-                Toast.makeText(this, R.string.nogps4google, Toast.LENGTH_LONG).show();
-            } else {
-                double lon = loc.getLongitude();
-                double lat = loc.getLatitude();
-                Intent gmapIntent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + lat + "," + lon)); //$NON-NLS-1$ //$NON-NLS-2$
-                startActivity(gmapIntent);
-            }
-
             return true;
         }
         return super.onMenuItemSelected(featureId, item);
