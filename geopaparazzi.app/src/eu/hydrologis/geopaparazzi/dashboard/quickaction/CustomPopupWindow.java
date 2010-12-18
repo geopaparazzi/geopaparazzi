824a7f5e7eedb4d8f8b91581b11811bad2fbb945
Andrea Antonello O <andrea.antonello@gmail.com>
2010-12-19 00:19:55 +0100

    fiddling around on quickaction pieces

--- geopaparazzi.app/src/eu/hydrologis/geopaparazzi/dashboard/quickaction/CustomPopupWindow.java
+++ geopaparazzi.app/src/eu/hydrologis/geopaparazzi/dashboard/quickaction/CustomPopupWindow.java
@@ -205,7 +205,8 @@ public class CustomPopupWindow {
 			window.setAnimationStyle(R.style.Animations_PopDownMenu_Center);
 		}
 
-		window.showAtLocation(anchor, Gravity.NO_GRAVITY, xPos, yPos);
+		window.showAtLocation(anchor, Gravity.LEFT, -10, -20);
+//		window.showAtLocation(anchor, Gravity.NO_GRAVITY, xPos, yPos);
 	}
 
 	public void dismiss() {
