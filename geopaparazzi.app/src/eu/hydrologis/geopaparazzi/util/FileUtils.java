ae01d63c6b8cdbcaeb8eecf0ab965fcb2a61294c
Andrea Antonello O <andrea.antonello@gmail.com>
2010-12-20 22:03:53 +0100

    add read method

--- geopaparazzi.app/src/eu/hydrologis/geopaparazzi/util/FileUtils.java
+++ geopaparazzi.app/src/eu/hydrologis/geopaparazzi/util/FileUtils.java
@@ -17,9 +17,12 @@
  */
 package eu.hydrologis.geopaparazzi.util;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -60,6 +63,27 @@ public class FileUtils {
         int lastDot = name.lastIndexOf("."); //$NON-NLS-1$
         name = name.substring(0, lastDot);
         return name;
+    }
+
+    public static String readfile( File file ) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            String line = null;
+            while( (line = br.readLine()) != null ) {
+                sb.append(line).append("\n");
+            }
+            return sb.toString();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     /**
