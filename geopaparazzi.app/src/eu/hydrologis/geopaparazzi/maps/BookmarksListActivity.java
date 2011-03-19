d469310ad56ac604fd093478dffe4ef7d0c5c292
Andrea Antonello O <andrea.antonello@gmail.com>
2011-03-19 07:24:56 +0100

    cleanup

--- geopaparazzi.app/src/eu/hydrologis/geopaparazzi/maps/BookmarksListActivity.java
+++ geopaparazzi.app/src/eu/hydrologis/geopaparazzi/maps/BookmarksListActivity.java
@@ -20,17 +20,15 @@ package eu.hydrologis.geopaparazzi.maps;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import android.app.AlertDialog;
-import android.app.ListActivity;
 import android.app.AlertDialog.Builder;
+import android.app.ListActivity;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.LayoutInflater;
@@ -38,17 +36,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 import eu.hydrologis.geopaparazzi.R;
 import eu.hydrologis.geopaparazzi.database.DaoBookmarks;
 import eu.hydrologis.geopaparazzi.util.Bookmark;
-import eu.hydrologis.geopaparazzi.util.Constants;
 import eu.hydrologis.geopaparazzi.util.debug.Logger;
 
 /**
