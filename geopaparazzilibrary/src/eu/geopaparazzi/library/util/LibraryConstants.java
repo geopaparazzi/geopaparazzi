40527e990ce917e315d72514d87140a4368179eb
Andrea Antonello M <andrea.antonello@gmail.com>
2012-02-24 12:11:22 +0100

    adding gpx export to library

--- geopaparazzilibrary/src/eu/geopaparazzi/library/util/LibraryConstants.java
+++ geopaparazzilibrary/src/eu/geopaparazzi/library/util/LibraryConstants.java
@@ -33,6 +33,7 @@ public interface LibraryConstants {
     public final SimpleDateFormat TIMESTAMPFORMATTER = new SimpleDateFormat("yyyyMMdd_HHmmss"); //$NON-NLS-1$
     public final SimpleDateFormat TIME_FORMATTER = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); //$NON-NLS-1$
     public final SimpleDateFormat TIME_FORMATTER_SQLITE = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); //$NON-NLS-1$
+    public final SimpleDateFormat TIME_FORMATTER_GPX = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'"); //$NON-NLS-1$
 
     /**
      * Key used by the gps logger to store the lat in the prefs. 
