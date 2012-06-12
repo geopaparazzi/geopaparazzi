f43922bf4cb519ef4a9a92a68e68ff7447e8d91d
Andrea Antonello M <andrea.antonello@gmail.com>
2012-06-12 14:58:15 +0200

    android 2.2, in order to not use features we do not have

--- geopaparazzilibrary/src/eu/geopaparazzi/library/camera/CameraActivity.java
+++ geopaparazzilibrary/src/eu/geopaparazzi/library/camera/CameraActivity.java
@@ -24,6 +24,7 @@ import java.util.Date;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
@@ -200,6 +201,14 @@ public class CameraActivity extends Activity {
             }
             finish();
         }
+    }
+
+    public String getRealPathFromURI( Uri contentUri ) {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
     }
 
 }
