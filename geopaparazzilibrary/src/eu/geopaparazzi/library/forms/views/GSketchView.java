cd159423469a85d8505642ae38b1c98fbe99f4ed
Andrea Antonello O <andrea.antonello@gmail.com>
2012-10-31 10:15:31 +0100

    cleanup

--- geopaparazzilibrary/src/eu/geopaparazzi/library/forms/views/GSketchView.java
+++ geopaparazzilibrary/src/eu/geopaparazzi/library/forms/views/GSketchView.java
@@ -36,7 +36,6 @@ import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -51,7 +50,7 @@ import eu.geopaparazzi.library.util.debug.Debug;
 import eu.geopaparazzi.library.util.debug.Logger;
 
 /**
- * A custom {@link EditText} view.
+ * A custom Sketch view.
  * 
  * @author Andrea Antonello (www.hydrologis.com)
  */
