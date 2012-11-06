e6b610b1d8b090c204d54b2bac1602c3a940be38
Andrea Antonello M <andrea.antonello@gmail.com>
2012-11-06 12:44:23 +0100

    starting with activity

--- geopaparazzilibrary/src/eu/geopaparazzi/library/nfc/NfcIdReaderActivity.java
+++ geopaparazzilibrary/src/eu/geopaparazzi/library/nfc/NfcIdReaderActivity.java
@@ -1,15 +1,73 @@
 package eu.geopaparazzi.library.nfc;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 import eu.geopaparazzi.library.R;
+import eu.geopaparazzi.library.util.debug.Debug;
+import eu.geopaparazzi.library.util.debug.Logger;
 
 public class NfcIdReaderActivity extends Activity {
+
+    private NfcAdapter nfcAdapter;
 
     @Override
     public void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc_id_reader);
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.scanning_progressbar);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (!nfcAdapter.isEnabled()) {
+            Toast.makeText(getApplicationContext(), "Please activate NFC and press Back to return to the application!",
+                    Toast.LENGTH_LONG).show();
+            startActivity(new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS));
+        }
+
     }
 
+    @Override
+    protected void onNewIntent( Intent intent ) {
+        // NDEF exchange mode
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            NdefMessage[] msgs = getNdefMessages(intent);
+
+        }
+
+    }
+
+    private NdefMessage[] getNdefMessages( Intent intent ) {
+        // Parse the intent
+        NdefMessage[] msgs = null;
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action) || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for( int i = 0; i < rawMsgs.length; i++ ) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            } else {
+                // Unknown tag type
+                byte[] empty = new byte[]{};
+                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
+                NdefMessage msg = new NdefMessage(new NdefRecord[]{record});
+                msgs = new NdefMessage[]{msg};
+            }
+        } else {
+            if (Debug.D)
+                Logger.d(this, "Unknown intent.");
+            finish();
+        }
+        return msgs;
+    }
 }
