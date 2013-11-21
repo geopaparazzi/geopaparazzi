7b552afe406edfd8cb5d0e76309045a1b1eaf845
Andrea Antonello <andrea.antonello@gmail.com>
2013-11-21 09:49:29 +0100

    changing some style parts

--- geopaparazzi.mapsforge/src/eu/geopaparazzi/mapsforge/mapsdirmanager/treeview/AbstractTreeViewAdapter.java
+++ geopaparazzi.mapsforge/src/eu/geopaparazzi/mapsforge/mapsdirmanager/treeview/AbstractTreeViewAdapter.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.database.DataSetObserver;
 import android.graphics.drawable.Drawable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
