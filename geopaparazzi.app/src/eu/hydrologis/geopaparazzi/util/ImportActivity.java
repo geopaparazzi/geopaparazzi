544af86f65c4689f1af172143aa06d7e3dd6e87e
Andrea Antonello <andrea.antonello@gmail.com>
2014-01-04 17:40:52 +0100

    allow for multiple bookmarks file to be imported by choice

--- geopaparazzi.app/src/eu/hydrologis/geopaparazzi/util/ImportActivity.java
+++ geopaparazzi.app/src/eu/hydrologis/geopaparazzi/util/ImportActivity.java
@@ -22,11 +22,14 @@ import static eu.hydrologis.geopaparazzi.util.Constants.PREF_KEY_SERVER;
 import static eu.hydrologis.geopaparazzi.util.Constants.PREF_KEY_USER;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.List;
 import java.util.TreeSet;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -40,9 +43,12 @@ import eu.geopaparazzi.library.util.LibraryConstants;
 import eu.geopaparazzi.library.util.ResourcesManager;
 import eu.geopaparazzi.library.util.Utilities;
 import eu.geopaparazzi.library.util.activities.DirectoryBrowserActivity;
+import eu.geopaparazzi.library.util.activities.GeocodeActivity;
+import eu.geopaparazzi.library.util.activities.InsertCoordActivity;
 import eu.geopaparazzi.library.webproject.WebProjectsListActivity;
 import eu.hydrologis.geopaparazzi.R;
 import eu.hydrologis.geopaparazzi.database.DaoBookmarks;
+import eu.hydrologis.geopaparazzi.maps.MapsActivity;
 
 /**
  * Activity for export tasks.
@@ -96,61 +102,7 @@ public class ImportActivity extends Activity {
         ImageButton bookmarksImportButton = (ImageButton) findViewById(R.id.bookmarksImportButton);
         bookmarksImportButton.setOnClickListener(new Button.OnClickListener(){
             public void onClick( View v ) {
-                final ImportActivity context = ImportActivity.this;
-
-                ResourcesManager resourcesManager = null;
-                try {
-                    resourcesManager = ResourcesManager.getInstance(context);
-                } catch (Exception e1) {
-                    e1.printStackTrace();
-                }
-                File sdcardDir = resourcesManager.getSdcardDir();
-                File bookmarksfile = new File(sdcardDir, "bookmarks.csv"); //$NON-NLS-1$
-                if (bookmarksfile.exists()) {
-                    try {
-                        // try to load it
-                        List<Bookmark> allBookmarks = DaoBookmarks.getAllBookmarks();
-                        TreeSet<String> bookmarksNames = new TreeSet<String>();
-                        for( Bookmark bookmark : allBookmarks ) {
-                            String tmpName = bookmark.getName();
-                            bookmarksNames.add(tmpName.trim());
-                        }
-
-                        List<String> bookmarksList = FileUtilities.readfileToList(bookmarksfile);
-                        int imported = 0;
-                        for( String bookmarkLine : bookmarksList ) {
-                            String[] split = bookmarkLine.split(","); //$NON-NLS-1$
-                            // bookmarks are of type: Agritur BeB In Valle, 45.46564, 11.58969, 12
-                            if (split.length < 3) {
-                                continue;
-                            }
-                            String name = split[0].trim();
-                            if (bookmarksNames.contains(name)) {
-                                continue;
-                            }
-                            try {
-                                double zoom = 16.0;
-                                if (split.length == 4) {
-                                    zoom = Double.parseDouble(split[3]);
-                                }
-                                double lat = Double.parseDouble(split[1]);
-                                double lon = Double.parseDouble(split[2]);
-                                DaoBookmarks.addBookmark(lon, lat, name, zoom, -1, -1, -1, -1);
-                                imported++;
-                            } catch (Exception e) {
-
-                                e.printStackTrace();
-                            }
-                        }
-
-                        Utilities.messageDialog(context, getString(R.string.successfully_imported_bookmarks) + imported, null);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        Utilities.messageDialog(context, R.string.error_bookmarks_import, null);
-                    }
-                } else {
-                    Utilities.messageDialog(context, R.string.no_bookmarks_csv, null);
-                }
+                importBookmarks();
             }
         });
     }
@@ -163,6 +115,93 @@ public class ImportActivity extends Activity {
         browseIntent.putExtra(DirectoryBrowserActivity.EXTENTION, ".gpx"); //$NON-NLS-1$
         startActivity(browseIntent);
         finish();
+    }
+
+    private void importBookmarks() {
+        final ImportActivity context = ImportActivity.this;
+
+        ResourcesManager resourcesManager = null;
+        try {
+            resourcesManager = ResourcesManager.getInstance(context);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        final File sdcardDir = resourcesManager.getSdcardDir();
+        File[] bookmarksfileList = sdcardDir.listFiles(new FilenameFilter(){
+            public boolean accept( File dir, String filename ) {
+                return filename.startsWith("bookmarks") && filename.endsWith(".csv");
+            }
+        });
+        if (bookmarksfileList.length == 0) {
+            Utilities.warningDialog(context, "No bookmarks file found.", null);
+            return;
+        }
+
+        final String[] items = new String[bookmarksfileList.length];
+        for( int i = 0; i < items.length; i++ ) {
+            items[i] = bookmarksfileList[i].getName();
+        }
+
+        new AlertDialog.Builder(this).setSingleChoiceItems(items, 0, null)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener(){
+
+                    public void onClick( DialogInterface dialog, int whichButton ) {
+                        int selectedPosition = ((AlertDialog) dialog).getListView().getCheckedItemPosition();
+                        String selectedItem = items[selectedPosition];
+                        dialog.dismiss();
+                        doImport(context, sdcardDir, selectedItem);
+                    }
+                }).show();
+
+    }
+
+    private void doImport( final ImportActivity context, File sdcardDir, String fileName ) {
+        File bookmarksfile = new File(sdcardDir, fileName); //$NON-NLS-1$
+        if (bookmarksfile.exists()) {
+            try {
+                // try to load it
+                List<Bookmark> allBookmarks = DaoBookmarks.getAllBookmarks();
+                TreeSet<String> bookmarksNames = new TreeSet<String>();
+                for( Bookmark bookmark : allBookmarks ) {
+                    String tmpName = bookmark.getName();
+                    bookmarksNames.add(tmpName.trim());
+                }
+
+                List<String> bookmarksList = FileUtilities.readfileToList(bookmarksfile);
+                int imported = 0;
+                for( String bookmarkLine : bookmarksList ) {
+                    String[] split = bookmarkLine.split(","); //$NON-NLS-1$
+                    // bookmarks are of type: Agritur BeB In Valle, 45.46564, 11.58969, 12
+                    if (split.length < 3) {
+                        continue;
+                    }
+                    String name = split[0].trim();
+                    if (bookmarksNames.contains(name)) {
+                        continue;
+                    }
+                    try {
+                        double zoom = 16.0;
+                        if (split.length == 4) {
+                            zoom = Double.parseDouble(split[3]);
+                        }
+                        double lat = Double.parseDouble(split[1]);
+                        double lon = Double.parseDouble(split[2]);
+                        DaoBookmarks.addBookmark(lon, lat, name, zoom, -1, -1, -1, -1);
+                        imported++;
+                    } catch (Exception e) {
+
+                        e.printStackTrace();
+                    }
+                }
+
+                Utilities.messageDialog(context, getString(R.string.successfully_imported_bookmarks) + imported, null);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Utilities.messageDialog(context, R.string.error_bookmarks_import, null);
+            }
+        } else {
+            Utilities.messageDialog(context, R.string.no_bookmarks_csv, null);
+        }
     }
 
 }
