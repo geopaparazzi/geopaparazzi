0a602fa729c06d465278950a2164f5fb43489c2f
Andrea Antonello <andrea.antonello@gmail.com>
2016-02-27 10:24:01 +0100

    cleanup of incomins sms - remove geomsms

--- geopaparazzilibrary/src/main/java/eu/geopaparazzi/library/sms/SmsReceiverPositionSender.java
+++ geopaparazzilibrary/src/main/java/eu/geopaparazzi/library/sms/SmsReceiverPositionSender.java
@@ -21,19 +21,22 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.Telephony;
 import android.telephony.SmsMessage;
+
 import eu.geopaparazzi.library.R;
 import eu.geopaparazzi.library.database.GPLog;
 import eu.geopaparazzi.library.util.LibraryConstants;
 
 /**
  * Sms reciever and gps coordinates sender.
- * 
- * <p>
+ * <p/>
+ * <p/>
  * To use this you will need to add to the manifest:
- * 
+ * <p/>
  * <pre>
  *      <receiver android:name="eu.geopaparazzi.library.sms.SmsReceiverPositionSender" android:enabled="true">
  *           <intent-filter>
@@ -41,7 +44,7 @@ import eu.geopaparazzi.library.util.LibraryConstants;
  *           </intent-filter>
  *       </receiver>
  * </pre>
- * 
+ *
  * @author Andrea Antonello (www.hydrologis.com)
  */
 @SuppressWarnings("nls")
@@ -49,7 +52,7 @@ public class SmsReceiverPositionSender extends BroadcastReceiver {
     private static final String SMS_REC_ACTION = "android.provider.Telephony.SMS_RECEIVED"; //$NON-NLS-1$
 
     @Override
-    public void onReceive( final Context context, Intent intent ) {
+    public void onReceive(final Context context, Intent intent) {
         if (intent.getAction().equals(SmsReceiverPositionSender.SMS_REC_ACTION)) {
 
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
@@ -58,47 +61,46 @@ public class SmsReceiverPositionSender extends BroadcastReceiver {
                 return;
             }
 
-            boolean isGeosms = false;
             boolean isGeopapsms = false;
 
             Bundle bundle = intent.getExtras();
-            SmsMessage smsMessage = null;
             String body = null;
+            SmsMessage smsMessage = null;
             if (bundle != null) {
-                Object[] pdus = (Object[]) bundle.get("pdus");
-                for( Object pdu : pdus ) {
-                    smsMessage = SmsMessage.createFromPdu((byte[]) pdu);
-                    body = smsMessage.getDisplayMessageBody();
-                    if (body == null) {
-                        continue;
+                if (Build.VERSION.SDK_INT >= 19) { //KITKAT
+                    SmsMessage[] msgs = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+                    smsMessage = msgs[0];
+                } else {
+                    Object tmp = bundle.get("pdus");
+                    if (tmp instanceof Object[]) {
+                        Object[] pdus = (Object[]) tmp;
+                        smsMessage = SmsMessage.createFromPdu((byte[]) pdus[0]);
                     }
+                }
+            }
+
+            if (smsMessage != null) {
+                body = smsMessage.getDisplayMessageBody();
+                if (body != null && smsMessage.getOriginatingAddress() != null) {
                     if (GPLog.LOG)
                         GPLog.addLogEntry(this, "Got message: " + body);
                     if (body.toLowerCase().matches(".*geopap.*")) {
                         isGeopapsms = true;
                     }
-                    if (body.toLowerCase().matches(".*geosms.*")) {
-                        isGeosms = true;
-                    }
-                }
-            }
-            if (body != null && smsMessage != null && smsMessage.getOriginatingAddress() != null) {
-                if (isGeopapsms) {
-                    String msg = null;
-                    String lastPosition = context.getString(R.string.last_position);
-                    msg = SmsUtilities.createPositionText(context, lastPosition);
-                    if (msg.length() > 160) {
-                        // if longer than 160 chars it will not work, try using only url
-                        msg = SmsUtilities.createPositionText(context, null);
-                    }
+                    if (isGeopapsms) {
+                        String msg = null;
+                        String lastPosition = context.getString(R.string.last_position);
+                        msg = SmsUtilities.createPositionText(context, lastPosition);
+                        if (msg.length() > 160) {
+                            // if longer than 160 chars it will not work, try using only url
+                            msg = SmsUtilities.createPositionText(context, null);
+                        }
 
-                    if (GPLog.LOG)
-                        GPLog.addLogEntry(this, msg);
-                    String addr = smsMessage.getOriginatingAddress();
-                    SmsUtilities.sendSMS(context, addr, msg, true);
-                }
-                if (isGeosms) {
-                    SmsUtilities.openGeoSms(context, body);
+                        if (GPLog.LOG)
+                            GPLog.addLogEntry(this, msg);
+                        String addr = smsMessage.getOriginatingAddress();
+                        SmsUtilities.sendSMS(context, addr, msg, true);
+                    }
                 }
             }
         }
